1c7be77974305e9ac4cc5ce583759fcc9a5562d3
PabloFP64 <PabloFP64@gmail.com>
2022-11-20 16:53:44 +0100

    Creado mas dbJAva

--- app/src/main/java/com/example/accesobd/DBHelper.java
+++ app/src/main/java/com/example/accesobd/DBHelper.java
@@ -1,4 +1,37 @@
 package com.example.accesobd;
 
-public class DBHelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static  final String DBNAME="login.db";
+    public DBHelper(Context context) {
+        super(context, "login.db", null, 1);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table users(username TEXT primary key, password TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int OldVersion, int newVersion) {
+        db.execSQL("drop table if exists users");
+
+    }
+
+    public Boolean insertData(String username, String password){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put("username", username);
+        values.put("password", password);
+
+        long result= db.insert("users", null, values);
+        if(result==-1) return false;
+        else
+            return true;
+    }
 }
