7a9cf644bad9f13b3d8a2e448fd64ab19b3dbc96
PabloFP64 <PabloFP64@gmail.com>
2022-11-20 16:37:14 +0100

    Avances main y creado DB

---
+++ app/src/main/java/com/example/accesobd/DBHelper.java
@@ -0,0 +1,4 @@
+package com.example.accesobd;
+
+public class DBHelper {
+}
--- app/src/main/java/com/example/accesobd/MainActivity.java
+++ app/src/main/java/com/example/accesobd/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.accesobd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username, password, password2;
+    Button BotonReg, BotonLog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username=findViewById(R.id.username);
+        password=findViewById(R.id.password);
+        //password2=findViewById(R.id.password2);
+        BotonLog=findViewById(R.id.BotonLog);
+        BotonReg=findViewById(R.id.BotonReg);
+
+        BotonReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        BotonLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
