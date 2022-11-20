8de8520fc9a997c44bd3ff287dee24890509b095
PabloFP64 <PabloFP64@gmail.com>
2022-11-20 17:17:22 +0100

    Creado home y añadido funcionalidad a MainActivity

--- app/src/main/java/com/example/accesobd/MainActivity.java
+++ app/src/main/java/com/example/accesobd/MainActivity.java
@@ -2,15 +2,20 @@ package com.example.accesobd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText username, password, password2;
     Button BotonReg, BotonLog;
+    DBHelper DB;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +27,36 @@ public class MainActivity extends AppCompatActivity {
         //password2=findViewById(R.id.password2);
         BotonLog=findViewById(R.id.BotonLog);
         BotonReg=findViewById(R.id.BotonReg);
+        DB= new DBHelper(this);
 
         BotonReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String user= username.getText().toString();
+                String pass= password.getText().toString();
+                String pass2= password2.getText().toString();
+
+                if(TextUtils.isEmpty(user) || TextUtils.isEmpty(pass) || TextUtils.isEmpty(pass2))
+                    Toast.makeText(MainActivity.this, "Rellena todo", Toast.LENGTH_SHORT).show();
+                else{
+                    if(pass.equals(pass2)){
+                        Boolean checkUser = DB.checkUsername(user);
+                        if(checkUser==false){
+                            Boolean insert = DB.insertData(user,pass);
+                            if(insert==true){
+                                Toast.makeText(MainActivity.this, "Registrado Correctamente", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
+                                startActivity(intent);
+                            }else{
+                                Toast.makeText(MainActivity.this, "Registro Fallido", Toast.LENGTH_SHORT).show();
+                            }
+                        }else{
+                            Toast.makeText(MainActivity.this, "Usuario Ya Existe", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(MainActivity.this,"Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+                    }
+                }
 
             }
         });
@@ -33,7 +64,8 @@ public class MainActivity extends AppCompatActivity {
         BotonLog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent= new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
             }
         });
     }
